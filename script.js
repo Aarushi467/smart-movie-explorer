d28d758013a3e15bc46f8d110ebefc03e0906771
Aarushi Singh <aarushisingh@Aarushis-MacBook-Pro.local>
2026-04-08 20:32:26 +0530

    saving current progress

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-async function searchMovies() {
-  const query = document.getElementById("searchInput").value;
-
-  const response = await fetch(`https://www.omdbapi.com/?s=${query}&apikey=YOUR_API_KEY`);
-  const data = await response.json();
-
-  const moviesDiv = document.getElementById("movies");
-  moviesDiv.innerHTML = "";
-
-  if (data.Search) {
-    data.Search.forEach(movie => {
-      const div = document.createElement("div");
-      div.classList.add("movie");
-
-      div.innerHTML = `
-        <h3>${movie.Title}</h3>
-        <img src="${movie.Poster}" />
-      `;
-
-      moviesDiv.appendChild(div);
-    });
-  } else {
-    moviesDiv.innerHTML = "<p>No results found</p>";
-  }
-}
-=======
 let allMovies = [];
 let favorites = JSON.parse(localStorage.getItem("fav")) || [];
 
@@ -148,4 +121,4 @@ function closeModal() {
   document.getElementById("modal").style.display = "none";
 }
 
->>>>>>> 1553344 (Milestone 3 complete)
+
